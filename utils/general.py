c3aef8f6b92228c786b0864b5e2cca250485affc
holan <gh.mss_by.hl@163.com>
2021-02-05 15:01:15 +0800

    fix: predict result

--- utils/general.py
+++ utils/general.py
@@ -3,7 +3,8 @@ import random
 import numpy as np
 import math
 import time
-
+import torch
+import torchvision
 from .torch_utils import init_torch_seeds
 
 
@@ -16,6 +17,15 @@ def init_seeds(seed=0):
   random.seed(seed)
   np.random.seed(seed)
   init_torch_seeds(seed)
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
 
 def non_max_suppression(prediction, conf_thres=0.1, iou_thres=0.6, merge=False, classes=None, agnostic=False):
     """Performs Non-Maximum Suppression (NMS) on inference results
